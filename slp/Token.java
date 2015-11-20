880b2cc4a40a806fa06ce06cde4a684dbc3ee091
Tomotz <tom.mottes@gmail.com>
2015-11-20 21:17:04 +0200

    cup

---
+++ slp/Token.java
@@ -0,0 +1,42 @@
+package slp;
+
+import java_cup.runtime.Symbol;
+
+/** Adds line number and name information to scanner symbols.
+ */
+public class Token extends Symbol {
+	private final int line;
+	private final String name;
+
+	public Token(int line, String name, int id, Object value) {
+		super(id, value);
+		this.name = name;
+		this.line = line + 1;
+	}
+
+	public Token(int m_type, int line, int column, Object value) {
+		super(m_type, line, column, value);
+		this.name = "";
+		this.line = line + 1;
+	}
+	public Token(int m_type, int line, int column) {
+		super(m_type, line, column);
+		this.name = "";
+		this.line = line + 1;
+	}
+
+	public Token(int line, String name, int id) {
+		super(id, null);
+		this.name = name;
+		this.line = line + 1;
+	}
+	
+	public String toString() {
+		String val = value != null ? "(" + value + ")" : "";
+		return name +  val;
+	}
+	
+	public int getLine() {
+		return line;
+	}
+}
