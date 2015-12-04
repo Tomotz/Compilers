963b4f500632ac5fa196f4742a2b91c27db7ef91
Tomotz <tom.mottes@gmail.com>
2015-12-04 16:20:42 +0200

    41215

---
+++ slp/icClass.java
@@ -0,0 +1,11 @@
+package slp;
+import java.util.ArrayList;
+import java.util.List;
+
+import slp.icObject;
+
+
+public class icClass extends icObject {
+	
+	List<String> statMethod = new ArrayList<String>();
+}
---
+++ slp/icFunction.java
@@ -0,0 +1,14 @@
+package slp;
+import java.util.ArrayList;
+import java.util.List;
+
+import slp.icObject;
+
+
+public class icFunction extends icObject {
+	
+	String retType;
+	List<String> varType = new ArrayList<String>();
+	List<String> methods = new ArrayList<String>();
+
+}
