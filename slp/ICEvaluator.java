59a0a47f1c4384f2396756259ee485a7377888e8
eden <edenilan4@gmail.com>
2015-12-22 10:04:24 +0200

    s

--- slp/ICEvaluator.java
+++ slp/ICEvaluator.java
@@ -240,6 +240,7 @@ public class ICEvaluator implements PropagatingVisitor<Environment, VarType> {
 				IR.add_line("Move 0,"+result); // if reached here: the whole expression is false
 			}else{ //op==LAND
 				IR.add_comment(lhs_reg+" && ...");
+
 				IR.add_line("Move 0,"+result); //result=1
 				IR.add_line("Move "+lhs_reg+","+temp1);
 				IR.add_line("Compare 0,"+temp1); //now compare=lhs-0
