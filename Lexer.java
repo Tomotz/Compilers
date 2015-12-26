ace4b011e0439ac94d03cf3bd6fcdd5988300cc1
eden <edenilan4@gmail.com>
2015-12-26 23:12:47 +0200

    labels fixed

--- Lexer.java
+++ Lexer.java
@@ -7,8 +7,11 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
 <<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -38,6 +41,9 @@ package slp;
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
