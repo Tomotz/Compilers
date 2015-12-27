f72118ecbeca3f375a976c06099b69ab39930b14
עומר <>
2015-12-27 22:04:28 +0200

    changes

--- slp/ICEvaluator.java
+++ slp/ICEvaluator.java
@@ -147,7 +147,9 @@ public class ICEvaluator implements PropagatingVisitor<Environment, VarType> {
 		validateAssign(varExpr_type, rhs_type, stmt, env);
 		
 		if (run_num ==1){
-			IR.move(varExpr_type.ir_val, rhs_type.ir_val,1,env);
+			if (!varExpr_type.type.equals("string")){
+				IR.move(varExpr_type.ir_val, rhs_type.ir_val,1,env);
+			}
 		}
 		return null;
 	}
@@ -639,16 +641,18 @@ public class ICEvaluator implements PropagatingVisitor<Environment, VarType> {
 			} else if (!(type_classo instanceof icClass)){
 				error(type + " is not a class name", stmt);
 			}
-			
-			type.ir_val = id + "_var_" + Integer.toString(vari);
+			if (!type.type.equals("string")){
+				type.ir_val = id + "_var_" + Integer.toString(vari);
+				}
 			++vari;
 			
 			// case of assignment
 			if (stmt.rhs != null) {
 				rhs = stmt.rhs.accept(this, env);
 				validateAssign(type, rhs, stmt, env);
-
-				IR.move(type.ir_val, rhs.ir_val,1,env);
+				if (!type.type.equals("string")){
+					IR.move(type.ir_val, rhs.ir_val,1,env);
+				}
 			}
 		}
 		stmtFlag = 0;
